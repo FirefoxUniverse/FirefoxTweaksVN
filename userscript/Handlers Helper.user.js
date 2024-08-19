ae7d3e0a834d4e26b91f02e0c759856d11da1bec
Gunir <134402102+gunir@users.noreply.github.com>
2024-08-19 14:18:17 +0700

    Update Handlers Helper.user.js

--- userscript/Handlers Helper.user.js
+++ userscript/Handlers Helper.user.js
@@ -4,7 +4,7 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_registerMenuCommand
-// @version     3.0
+// @version     3.1
 // @author      -
 // @description Helper for protocol_hook.lua
 // @namespace Violentmonkey Scripts
@@ -320,17 +320,16 @@ if (document.domain == 'www.youtube.com' || document.domain == 'm.youtube.com') 
     }
     function addMenuCommand(s, url, b) {
         GM_registerMenuCommand(s, function () {
-            location.replace(url);
             if (url.indexOf('m.youtube.com') != -1) {
               GM_setValue('hh_mobile', true);
             } else if (url.indexOf('www.youtube.com') != -1) {
               GM_setValue('hh_mobile', false);
             }
+            location.replace(url);
         });
     }
     if (dmc == 'w') {
         addMenuCommand("Switch to YouTube Mobile persistently", "https://m.youtube.com/?persist_app=1&app=m", true);
-
     } else if (dmc == 'm') {
         addMenuCommand("Switch to YouTube Dekstop persistently", "http://www.youtube.com/?persist_app=1&app=desktop", false);
     }
