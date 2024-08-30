7d0618050b5f90b51393cdd448a8e3b9744b6aa7
Gunir <134402102+gunir@users.noreply.github.com>
2024-08-30 22:10:26 +0700

    Update user.js

--- user.js
+++ user.js
@@ -232,6 +232,9 @@ user_pref("full-screen-api.transition-duration.leave", "0 0");
 // Modal-less mpv:// handler
 user_pref("network.protocol-handler.external.mpv", true);
 
+/** Image Decode Buffer Size ***/
+user_pref("image.mem.decode_bytes_at_a_time", 32768);
+
 /****************************************************************************
  * Betterfox                                                                *
  * "Ad meliora"                                                             *
@@ -242,32 +245,6 @@ user_pref("network.protocol-handler.external.mpv", true);
 /****************************************************************************
  * SECTION: FASTFOX                                                         *
 ****************************************************************************/
-/** GENERAL ***/
-user_pref("content.notify.interval", 100000);
-
-/** GFX ***/
-user_pref("gfx.canvas.accelerated.cache-items", 4096);
-user_pref("gfx.canvas.accelerated.cache-size", 512);
-user_pref("gfx.content.skia-font-cache-size", 20);
-
-/** DISK CACHE ***/
-user_pref("browser.cache.jsbc_compression_level", 3);
-
-/** MEDIA CACHE ***/
-user_pref("media.memory_cache_max_size", 65536);
-user_pref("media.cache_readahead_limit", 7200);
-user_pref("media.cache_resume_threshold", 3600);
-
-/** IMAGE CACHE ***/
-user_pref("image.mem.decode_bytes_at_a_time", 32768);
-
-/** NETWORK ***/
-user_pref("network.http.max-connections", 1800);
-user_pref("network.http.max-persistent-connections-per-server", 10);
-user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
-user_pref("network.http.pacing.requests.enabled", false);
-user_pref("network.dnsCacheExpiration", 3600);
-user_pref("network.ssl_tokens_cache_capacity", 10240);
 
 /** TELEMETRY ***/
 user_pref("datareporting.policy.dataSubmissionEnabled", false);
